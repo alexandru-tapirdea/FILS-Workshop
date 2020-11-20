8a743fd302739050f4a13c949acc532cf8a50e7b
mustafa.baaj <mustafa.baaj@toyota.com>
2020-11-20 11:23:50 +0200

    Added new route and refactored code

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,9 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = 3000;
 
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -34,13 +34,13 @@ const writeFile = async (filePath, dataToWrite) => {
 }
 
 app.get("/tasks/getAll", async (req, res) => {
-    const readFile = await readFromFile('tasks.json');
-    res.json(readFile);
+    const tasksList = await readFromFile('tasks.json');
+    res.json(tasksList);
 });
 
 app.post("/tasks/createTask", async (req, res) => {
     let itemIds = uuidv4();
-    const readFile = await readFromFile('tasks.json');
+    const tasksList = await readFromFile('tasks.json');
 
     if (!req.body.title || !req.body.deadline || !req.body.priority || !req.body.category) {
         return res.status(400).send({
@@ -53,21 +53,40 @@ app.post("/tasks/createTask", async (req, res) => {
         deadline: req.body.deadline,
         priority: req.body.priority,
         category: req.body.category,
+        isCompleted: false
     }
 
-    readFile.push(toDo);
-    writeFile('tasks.json', readFile)
+    tasksList.push(toDo);
+    writeFile('tasks.json', tasksList);
 
-    res.status(201).send(readFile);
+    res.status(201).send(tasksList);
 });
+
+app.put("/tasks/setstatus/:id", async (req, res) => {
+    const tasksList = await readFromFile('tasks.json');
+
+
+    const filterTask = tasksList.findIndex(task => task.id === req.params.id);
+
+
+    if (filterTask === -1) {
+        res.status(404).send({
+            error: "Task Not found!"
+        });
+    }
+
+    tasksList[filterTask].isCompleted = true;
+
+    await writeFile('tasks.json', tasksList);
+
+    res.status(200).send(tasksList[filterTask]);
+})
 
 app.delete("/tasks/delete/:id", async (req, res) => {
     try {
-        const readFile = await readFromFile('tasks.json');
+        const tasksList = await readFromFile('tasks.json');
 
-        let found = readFile.find((task) => {
-            return task.id === req.params.id
-        });
+        let found = tasksList.find((task) => task.id === req.params.id);
 
         if (!found) {
             res.status(404).send({
@@ -75,10 +94,10 @@ app.delete("/tasks/delete/:id", async (req, res) => {
             });
         }
 
-        const arrayWithoutDeletedTask = readFile.filter(task => task.id !== req.params.id);
+        const arrayWithoutDeletedTask = tasksList.filter(task => task.id !== req.params.id);
         await writeFile('tasks.json', arrayWithoutDeletedTask);
 
-        res.status(200).send(arrayWithoutDeletedTask)
+        res.status(200).send(found);
     } catch (error) {
         res.status(500).send({ error: error.message });
     }
